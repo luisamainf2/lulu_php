f8d68c61182d67a892b50b5bc3520c2103562613
Luisa Machado Alberti <luisamainf2@gmail.com>
2025-10-06 14:11:05 -0300

    oi mundo chato

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+    <?php
+        include "./assets/includes/header.php";
+        require "./assets/includes/navegationMenu.php";
+    ?>
+    </body>
+</html>
